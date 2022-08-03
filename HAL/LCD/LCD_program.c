60a7b24b895ed494e1aee39860b965bea075772e
LeenaAlmekkawy <75577712+LeenaAlmekkawy@users.noreply.github.com>
2022-08-03 18:27:53 +0200

    Update LCD_program.c

--- HAL/LCD/LCD_program.c
+++ HAL/LCD/LCD_program.c
@@ -12,48 +12,48 @@
 void LCD_voidSendCommand(u8 Copy_u8Command)
 {
 #if LCD_MODE==EIGHT_BIT
-	GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_RS_PIN,LOW);
+    GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_RS_PIN,LOW);
 
-	GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_RW_PIN,LOW);
-     for(u8 i=0;i<8;i++)
-   {
-	   GPIO_voidSetPinValue(LCD_DATA_PORT,i,GET_BIT(Copy_u8Command,i));
-   }
+    GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_RW_PIN,LOW);
+    for(u8 i=0; i<8; i++)
+    {
+        GPIO_voidSetPinValue(LCD_DATA_PORT,i,GET_BIT(Copy_u8Command,i));
+    }
 
-   GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_EN_PIN,HIGH);
-   _delay_ms(2);
-   GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_EN_PIN,LOW);
+    GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_EN_PIN,HIGH);
+    _delay_ms(2);
+    GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_EN_PIN,LOW);
 #elif LCD_MODE==FOUR_BIT
 
-   GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_RS_PIN,LOW);
+    GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_RS_PIN,LOW);
 
-   	GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_RW_PIN,LOW);
-   	u8 Local_command1=(Copy_u8Command);
-    for(u8 i=4;i<8;i++)
-      {
-   	   GPIO_voidSetPinValue(LCD_DATA_PORT,i,GET_BIT(Local_command1,i));
-      }
+    GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_RW_PIN,LOW);
+    u8 Local_command1=(Copy_u8Command);
+    for(u8 i=4; i<8; i++)
+    {
+        GPIO_voidSetPinValue(LCD_DATA_PORT,i,GET_BIT(Local_command1,i));
+    }
     GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_EN_PIN,HIGH);
-       _delay_ms(2);
-       GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_EN_PIN,LOW);
-      _delay_ms(1);
-  	 Local_command1=(Copy_u8Command<<4);
-  	  GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_RS_PIN,LOW);
+    _delay_ms(2);
+    GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_EN_PIN,LOW);
+    _delay_ms(1);
+    Local_command1=(Copy_u8Command<<4);
+    GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_RS_PIN,LOW);
 
-  	   	GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_RW_PIN,LOW);
+    GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_RW_PIN,LOW);
 
-    for(u8 i=4;i<8;i++)
-      {
-   	   GPIO_voidSetPinValue(LCD_DATA_PORT,i,GET_BIT(Local_command1,i));
-      }
+    for(u8 i=4; i<8; i++)
+    {
+        GPIO_voidSetPinValue(LCD_DATA_PORT,i,GET_BIT(Local_command1,i));
+    }
 
     GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_EN_PIN,HIGH);
-      _delay_ms(2);
-      GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_EN_PIN,LOW);
+    _delay_ms(2);
+    GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_EN_PIN,LOW);
 
 #endif
 
-   
+
 }
 
 
@@ -62,45 +62,45 @@ void LCD_voidSendCommand(u8 Copy_u8Command)
 void LCD_voidSendData(u8 Copy_u8Data)
 {
 #if LCD_MODE==EIGHT_BIT
-	GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_RS_PIN,HIGH);
+    GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_RS_PIN,HIGH);
 
-	GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_RW_PIN,LOW);
-   for(u8 i=0;i<8;i++)
-   {
-	   GPIO_voidSetPinValue(LCD_DATA_PORT,i,GET_BIT(Copy_u8Data,i));
-   }
+    GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_RW_PIN,LOW);
+    for(u8 i=0; i<8; i++)
+    {
+        GPIO_voidSetPinValue(LCD_DATA_PORT,i,GET_BIT(Copy_u8Data,i));
+    }
 
 ///------------------------------------------------
-   GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_EN_PIN,HIGH);
-   _delay_ms(2);
-   GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_EN_PIN,LOW);
+    GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_EN_PIN,HIGH);
+    _delay_ms(2);
+    GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_EN_PIN,LOW);
 
 #elif LCD_MODE==FOUR_BIT
 
-	GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_RS_PIN,HIGH);
-	GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_RW_PIN,LOW);
- 	u8 Local_data1=(Copy_u8Data);
-  for(u8 i=4;i<8;i++)
-  {
-	   GPIO_voidSetPinValue(LCD_DATA_PORT,i,GET_BIT(Local_data1,i));
-  }
-  GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_EN_PIN,HIGH);
-   _delay_ms(2);
-   GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_EN_PIN,LOW);
-   _delay_ms(1);
-	 Local_data1=(Copy_u8Data<<4);
+    GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_RS_PIN,HIGH);
+    GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_RW_PIN,LOW);
+    u8 Local_data1=(Copy_u8Data);
+    for(u8 i=4; i<8; i++)
+    {
+        GPIO_voidSetPinValue(LCD_DATA_PORT,i,GET_BIT(Local_data1,i));
+    }
+    GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_EN_PIN,HIGH);
+    _delay_ms(2);
+    GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_EN_PIN,LOW);
+    _delay_ms(1);
+    Local_data1=(Copy_u8Data<<4);
 
-	 GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_RS_PIN,HIGH);
+    GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_RS_PIN,HIGH);
 
-	 	GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_RW_PIN,LOW);
- for(u8 i=4;i<8;i++)
- {
-	   GPIO_voidSetPinValue(LCD_DATA_PORT,i,GET_BIT(Local_data1,i));
- }
+    GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_RW_PIN,LOW);
+    for(u8 i=4; i<8; i++)
+    {
+        GPIO_voidSetPinValue(LCD_DATA_PORT,i,GET_BIT(Local_data1,i));
+    }
 ///------------------------------------------------
-  GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_EN_PIN,HIGH);
-  _delay_ms(2);
-  GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_EN_PIN,LOW);
+    GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_EN_PIN,HIGH);
+    _delay_ms(2);
+    GPIO_voidSetPinValue(LCD_CTRL_PORT,LCD_EN_PIN,LOW);
 
 #endif
 
@@ -113,49 +113,49 @@ void LCD_voidSendData(u8 Copy_u8Data)
 void LCD_voidInit(void)
 {
 #if LCD_MODE==EIGHT_BIT
-	PIN_st LCD={OUTPUT,PP,HS,NPP};
- for(u8 i=0;i<8;i++)
- {
-	GPIO_voidSetPinDirection (LCD_DATA_PORT,i,&LCD);
- }
+    PIN_st LCD= {OUTPUT,PP,HS,NPP};
+    for(u8 i=0; i<8; i++)
+    {
+        GPIO_voidSetPinDirection (LCD_DATA_PORT,i,&LCD);
+    }
 
 
- GPIO_voidSetPinDirection(LCD_CTRL_PORT,LCD_RS_PIN,&LCD);
+    GPIO_voidSetPinDirection(LCD_CTRL_PORT,LCD_RS_PIN,&LCD);
 
- GPIO_voidSetPinDirection(LCD_CTRL_PORT,LCD_RW_PIN,&LCD);
+    GPIO_voidSetPinDirection(LCD_CTRL_PORT,LCD_RW_PIN,&LCD);
 
- GPIO_voidSetPinDirection(LCD_CTRL_PORT,LCD_EN_PIN,&LCD);
+    GPIO_voidSetPinDirection(LCD_CTRL_PORT,LCD_EN_PIN,&LCD);
 
- _delay_ms(40);
+    _delay_ms(40);
 
- LCD_voidSendCommand(0x38);
+    LCD_voidSendCommand(0x38);
 
- LCD_voidSendCommand(0b00001100);
+    LCD_voidSendCommand(0b00001100);
 
- LCD_voidSendCommand(0x01);
+    LCD_voidSendCommand(0x01);
 #elif LCD_MODE==FOUR_BIT
 
- PIN_st LCD={OUTPUT,PP,HS,NPP};
-  for(u8 i=4;i<8;i++)
-  {
- 	GPIO_voidSetPinDirection (LCD_DATA_PORT,i,&LCD);
-  }
+    PIN_st LCD= {OUTPUT,PP,HS,NPP};
+    for(u8 i=4; i<8; i++)
+    {
+        GPIO_voidSetPinDirection (LCD_DATA_PORT,i,&LCD);
+    }
 
 
-  GPIO_voidSetPinDirection(LCD_CTRL_PORT,LCD_RS_PIN,&LCD);
+    GPIO_voidSetPinDirection(LCD_CTRL_PORT,LCD_RS_PIN,&LCD);
 
-  GPIO_voidSetPinDirection(LCD_CTRL_PORT,LCD_RW_PIN,&LCD);
+    GPIO_voidSetPinDirection(LCD_CTRL_PORT,LCD_RW_PIN,&LCD);
 
-  GPIO_voidSetPinDirection(LCD_CTRL_PORT,LCD_EN_PIN,&LCD);
-  _delay_ms(40);
+    GPIO_voidSetPinDirection(LCD_CTRL_PORT,LCD_EN_PIN,&LCD);
+    _delay_ms(40);
 
-  ///-------------------
-  LCD_voidSendCommand(0x02);
-  LCD_voidSendCommand(0x28);
-  LCD_voidSendCommand(0b00001100);
+    ///-------------------
+    LCD_voidSendCommand(0x02);
+    LCD_voidSendCommand(0x28);
+    LCD_voidSendCommand(0b00001100);
 
-   LCD_voidSendCommand(0x01);
- //---------------------------------
+    LCD_voidSendCommand(0x01);
+//---------------------------------
 
 
 
@@ -165,78 +165,79 @@ void LCD_voidInit(void)
 
 }
 
-void LCD_voidGoToXY(u8 Copy_u8XPos ,u8 Copy_u8YPos)
+void LCD_voidGoToXY(u8 Copy_u8XPos,u8 Copy_u8YPos)
 {
-	u8 Local_u8Address =0;
-	if(Copy_u8XPos==0)
-	{
-		Local_u8Address=Copy_u8YPos;
-	}
+    u8 Local_u8Address =0;
+    if(Copy_u8XPos==0)
+    {
+        Local_u8Address=Copy_u8YPos;
+    }
 
-	else if(Copy_u8XPos==1)
-	{
-		Local_u8Address= DDRAM_OFFSET + Copy_u8YPos;
-	}
-	SET_BIT(Local_u8Address,7);
-	LCD_voidSendCommand(Local_u8Address);
+    else if(Copy_u8XPos==1)
+    {
+        Local_u8Address= DDRAM_OFFSET + Copy_u8YPos;
+    }
+    SET_BIT(Local_u8Address,7);
+    LCD_voidSendCommand(Local_u8Address);
 
 
 }
 void LCD_voidClear(void)
 {
-	 LCD_voidSendCommand(0b00000001);
+    LCD_voidSendCommand(0b00000001);
 }
 
 void LCD_voidSendCustomCharacter(u8*Copy_pu8CharacterArray,u8 Copy_u8PatternNum, u8 Copy_u8XPos,u8 Copy_u8YPos )
-{   u8 Local_u8LoopCounter;
-	u8 Local_u8CGRAMAddress=0;
-	Local_u8CGRAMAddress=Copy_u8PatternNum*8;
-	SET_BIT(Local_u8CGRAMAddress,6);
-	LCD_voidSendCommand(Local_u8CGRAMAddress);
-     for(Local_u8LoopCounter=0 ;Local_u8LoopCounter<8 ;Local_u8LoopCounter++)
-     {
-    	 LCD_voidSendData(Copy_pu8CharacterArray[Local_u8LoopCounter]);
+{
+    u8 Local_u8LoopCounter;
+    u8 Local_u8CGRAMAddress=0;
+    Local_u8CGRAMAddress=Copy_u8PatternNum*8;
+    SET_BIT(Local_u8CGRAMAddress,6);
+    LCD_voidSendCommand(Local_u8CGRAMAddress);
+    for(Local_u8LoopCounter=0 ; Local_u8LoopCounter<8 ; Local_u8LoopCounter++)
+    {
+        LCD_voidSendData(Copy_pu8CharacterArray[Local_u8LoopCounter]);
 
-     }
-     LCD_voidGoToXY(Copy_u8XPos , Copy_u8YPos);
-     LCD_voidSendData(Copy_u8PatternNum);
+    }
+    LCD_voidGoToXY(Copy_u8XPos, Copy_u8YPos);
+    LCD_voidSendData(Copy_u8PatternNum);
 
 }
 
 void LCD_voidSendString(const char* Copy_pcString)
 {
-  u8 Local_u8Counter=0;
-  while(Copy_pcString[Local_u8Counter]!='\0')
-  {
-	  LCD_voidSendData(Copy_pcString[Local_u8Counter]);
-	  Local_u8Counter++;
-  }
+    u8 Local_u8Counter=0;
+    while(Copy_pcString[Local_u8Counter]!='\0')
+    {
+        LCD_voidSendData(Copy_pcString[Local_u8Counter]);
+        Local_u8Counter++;
+    }
 }
 void LCD_voidWriteNumber(u64 Copy_u64num, u8 Copy_u8XPos,u8 Copy_u8Ypos)
-{ u8 Local_Digit=0;
-  u8 Local_Counter=0;
+{
+    u8 Local_Digit=0;
+    u8 Local_Counter=0;
 
 
-   u8 Digits[MAX_SIZE];
-	while(Copy_u64num>0)
-	{
-		Local_Digit=Copy_u64num%10;
-		Digits[Local_Counter]=Local_Digit;
-		Local_Counter++;
+    u8 Digits[MAX_SIZE];
+    while(Copy_u64num>0)
+    {
+        Local_Digit=Copy_u64num%10;
+        Digits[Local_Counter]=Local_Digit;
+        Local_Counter++;
 
-		Copy_u64num/=10;
-	}
+        Copy_u64num/=10;
+    }
 
-	 u8 Local_Iterator;
-	 u8 Local_Iterator2=Local_Counter-1;
+    u8 Local_Iterator;
+    u8 Local_Iterator2=Local_Counter-1;
 
-    for(Local_Iterator=0;Local_Iterator<Local_Counter ; Local_Iterator++)
-	{
-		 LCD_voidGoToXY(Copy_u8XPos,Copy_u8Ypos+Local_Iterator2);
-		 u8 Data=Digits[Local_Iterator]+48;
-		 LCD_voidSendData(Data);
-		 Local_Iterator2--;
+    for(Local_Iterator=0; Local_Iterator<Local_Counter ; Local_Iterator++)
+    {
+        LCD_voidGoToXY(Copy_u8XPos,Copy_u8Ypos+Local_Iterator2);
+        u8 Data=Digits[Local_Iterator]+48;
+        LCD_voidSendData(Data);
+        Local_Iterator2--;
 
-	}
+    }
 }
-
